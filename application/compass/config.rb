ef9b1868130e8811b17459b1c9c53a6a4c694f16
Jens Ohlig <jens.ohlig@gmail.com>
2011-06-20 01:21:21 +0200

    Added fonts dir.

--- application/compass/config.rb
+++ application/compass/config.rb
@@ -6,6 +6,7 @@ css_dir = "../static/css"
 sass_dir = "sass"
 images_dir = "../static/img"
 javascripts_dir = "../static/js"
+fonts_dir = "../static/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
