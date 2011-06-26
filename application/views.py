bc91247e3617550c9648040aaba7402768d3cf41
Jens Ohlig <jens.ohlig@gmail.com>
2011-06-27 01:05:52 +0200

    Baby steps...

--- application/views.py
+++ application/views.py
@@ -10,35 +10,16 @@ from google.appengine.api import users
 
 from flask import render_template, flash, url_for, redirect
 
-from models import ExampleModel
-from decorators import login_required, admin_required
-from forms import ExampleForm
+from models import QuestionModel
+#from forms import ExampleForm
 
 
 def home():
     return render_template('index.html')
-
-# 
-# @login_required
-# def new_example():
-#     """Add a new example"""
-#     form = ExampleForm()
-#     if form.validate_on_submit():
-#         example = ExampleModel(
-#                     example_id = form.example_id.data,
-#                     example_title = form.example_title.data,
-#                     added_by = users.get_current_user()
-#                     )
-#         example.put()
-#         flash(u'Example successfully saved.', 'success')
-#         return redirect(url_for('list_examples'))
-#     return render_template('new_example.html', form=form)
-# 
-# 
-# @admin_required
-# def admin_only():
-#     """This view requires an admin account"""
-#     return 'Super-seekrit admin page.'
+    
+def add_question(request):
+    """Add a question to the database"""
+    
 
 
 def warmup():
