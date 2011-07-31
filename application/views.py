bce2b95cd9eeaf12ad59172d98ca2ccb09a899c9
Jens Ohlig <jens.ohlig@gmail.com>
2011-07-31 07:15:28 +0200

    Added next question.

--- application/views.py
+++ application/views.py
@@ -9,11 +9,11 @@ from flask import render_template, url_for, redirect, request
 from models import Question
 from decimal import Decimal
 from types import *
+import random
 
 def home():
     questions = Question.gql( "WHERE aproved = 'yes'" ).fetch(limit=100)
     if (not questions == None):
-        import random
         random.shuffle(questions)
         return render_template('index.html', questions=questions)
     else:
@@ -25,14 +25,18 @@ def add_question():
         q = Question(question=request.form['entry'], aproved="yes")
         q.save()
     return redirect(url_for('home'))
-
-def question():
-    """template for question"""
-    return render_template('question_clean.html')
+    
+def random_question(qid=None):
+    """Return a random question that isn't the one this was linked from"""
+    questions = Question.gql( "WHERE aproved = 'yes'" ).fetch(limit=100)
+    if (not questions == None):
+        questions = [question for question in questions if question.key() != qid]
+        random.shuffle(questions)
+        question = questions.pop()
+        return redirect("/question/%s/" % question.key())
 
 def question_result(qid=None):
-    """template for question_result"""
-    if (qid):
+    if ('vote_value' in request.form):
         agree = "didn't tell us if you agree or disagree"
         question = Question.get(qid)
         if (question.total == None):
@@ -41,14 +45,13 @@ def question_result(qid=None):
             question.no = 0
         if (question.yes == None):
             question.yes = 0
-        if ('vote_value' in request.form):
-            question.total = question.total + 1
-            if (request.form['vote_value'] == "yes"):
-                question.yes = question.yes +1
-                agree = "disagree"
-            if (request.form['vote_value'] == "no"):
-                question.no = question.no +1
-                agree = "agree"
+        question.total = question.total + 1
+        if (request.form['vote_value'] == "yes"):
+            question.yes = question.yes +1
+            agree = "disagree"
+        if (request.form['vote_value'] == "no"):
+            question.no = question.no +1
+            agree = "agree"
         question.save()
         question = Question.get(qid)
         percent = 0
@@ -59,7 +62,8 @@ def question_result(qid=None):
             stylepercent = "style-" + str(int(percent))
         return render_template('question_result.html', qid=qid, question=question.question, percent=percent, total=question.total, agreed=agree, stylepercent=stylepercent)
     else:
-        return render_template('question_result.html')
+        question = Question.get(qid)
+        return render_template('question_clean.html', qid=qid, question=question.question)
 
 def leaderboard():
     """template for leaderboard"""
